5365f1e090671c2bf5284e38f40cdf2db0f24796
TanyaJRB <tanya.rashid@hotmail.com>
2024-06-05 23:58:19 +0100

    home page and nav bar

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import NavButton from './NavButton.vue';
+
+</script>
+
+<template>
+
+  <div className="bg-gradient-to-tr from-red-500 to-purple-400 relative h-screen w-screen">
+      <img
+        className="absolute inset-0 w-full h-full object-cover mix-blend-multiply filter brightness-50"
+        alt="main background image"
+        src="/src/assets/background.jpg"
+      />
+
+      <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center font-mono text-white">
+          <div className="space-y-8">
+            <div className="font-extrabold text-3xl sm:text-4xl md:text-5xl md:leading-tight">
+             Sonic Soul Tribe
+            </div>
+            <div className="font-secondary text-palette-light md:text-lg lg:text-xl">
+              We celebrate life through the creation and expression of music, dance and art. 
+            </div>
+            <div class="flex flex-row justify-center gap-x-10 border-2 border-slate-300 rounded-md text-xs md:text-md">
+              <NavButton button-text="Culture"/>
+              <NavButton button-text="Burning Man"/>
+              <NavButton button-text="Campers"/>
+              <NavButton button-text="Media"/>
+            </div>
+          </div>
+      
+      </div>
+    </div>
+
+</template>
+
---
+++ src/components/NavButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+interface NavButtonProps {
+  buttonText?: string;
+}
+
+interface NavButtonEvents {
+  (eventName: 'click', event: MouseEvent): void;
+}
+
+const props = defineProps<NavButtonProps>();
+const emit = defineEmits<NavButtonEvents>();
+
+function handleClick(event: MouseEvent): void {
+  emit('click', event);
+}
+
+</script>
+
+<template>
+    <button class="text-slate-200 hover:text-blue-300 hover:cursor-pointer p-2" @click="handleClick">{{ props.buttonText }}</button>
+</template>
+
