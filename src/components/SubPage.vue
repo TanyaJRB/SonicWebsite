f6f030136c2563dd523490480e2fac17b6e6996d
TanyaJRB <tanya.rashid@hotmail.com>
2024-07-12 19:50:39 +0100

    nav bar improvement

--- src/components/SubPage.vue
+++ src/components/SubPage.vue
@@ -23,7 +23,7 @@ import SocialMediaBar from "./Contact/SocialMediaBar.vue";
   <div class="flex flex-col relative min-h-screen">
     <div class="w-screen flex flex-row justify-between bg-slate-200/10">
       <SonicLogo class="m-1 w-[8%]" />
-      <NavBar class="items-center md:text-md lg:text-lg" />
+      <NavBar applyButtonClass class="items-center md:text-md lg:text-lg" />
       <SocialMediaBar class="mr-4" />
     </div>
     <slot />
